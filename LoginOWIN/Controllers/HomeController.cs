414d2491c9ebbdf5ac87232ae90793c4fbbdd613
Welivelton Oliveira <woliveira2699@outlook.com>
2019-06-17 01:48:18 -0300

    Ajustes

--- LoginOWIN/Controllers/HomeController.cs
+++ LoginOWIN/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace LoginOWIN.App_Start
+namespace LoginOWIN.Controllers
 {
     [Authorize]
     public class HomeController : Controller
