656394eb36651d520252a626299f900b10a48441
Welivelton Oliveira <woliveira2699@outlook.com>
2019-06-17 01:42:10 -0300

    Adicionar arquivos de projeto.

---
+++ LoginOWIN/App_Start/Startup.Auth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace LoginOWIN
+{
+    public partial class Startup
+    {
+        // Para obter mais informações sobre a autenticação de configuração, visite https://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Account/Login"),
+                AuthenticationMode = AuthenticationMode.Active
+                //,
+                //Provider = new CookieAuthenticationProvider
+                //{
+                //    // Permite que o aplicativo valide o carimbo de segurança quando o usuário efetuar login.
+                //    // Este é um recurso de segurança que é usado quando você altera uma senha ou adiciona um login externo à sua conta.  
+                //    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                //        validateInterval: TimeSpan.FromMinutes(30),
+                //        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                //}
+            });            
+        }
+    }
+}
---
+++ LoginOWIN/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using LoginOWIN.Repository;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginOWIN.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        private LoginContext db = new LoginContext();
+
+        public ActionResult Login() => View();
+
+        [HttpPost]
+        public ActionResult Login(string username, string returnUrl)
+        {
+
+            //var user = _userService.GetByEmail(model.Email);
+            var user = db.Usuario.FirstOrDefault(u => u.Usuario1 == username);
+
+            //check username and password from database, naive checking: 
+            //password should be in SHA
+            if (user != null)
+            {
+                var claims = new[] {
+                new Claim(ClaimTypes.Role, user.Cargo),
+                new Claim(ClaimTypes.Name, user.Usuario1),
+                // can add more claims
+            };
+
+                var identity = new ClaimsIdentity(claims, "ApplicationCookie");
+
+                // Add roles into claims
+                //var roles = _roleService.GetByUserId(user.Id);
+                //var roles = _roleService.GetByUserId(user.Id);
+                //if (roles.Any())
+                //{
+                //    var roleClaims = roles.Select(r => new Claim(ClaimTypes.Role, r.Name));
+                //    identity.AddClaims(roleClaims);
+                //}
+
+                var context = Request.GetOwinContext();
+                var authManager = context.Authentication;
+
+                authManager.SignIn(new AuthenticationProperties
+                { IsPersistent = false }, identity);
+
+                return RedirectToAction("Index", "Home");
+
+            }
+
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
